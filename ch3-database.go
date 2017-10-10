80aa26e82cc5249cafc6e2b45f76005fa332ae34
Beth Tenorio <betsky98@gmail.com>
2017-10-09 22:07:03 -0400

    connect to a mysql db and display a record

---
+++ ch3-database.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+  "database/sql"
+  "fmt"
+  _ "github.com/go-sql-driver/mysql"
+  "github.com/gorilla/mux"
+  "log"
+  "net/http"
+)
+
+const (
+  DBHost = "127.0.0.1"
+  DBPort = ":3306"
+  DBUser = "root"
+  DBPass = ""
+  DBDbase = "cms"
+  PORT = ":8080"
+)
+
+var database *sql.DB
+
+type Page struct {
+  Title string
+  Content string
+  Date string
+}
+
+func ServePage(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  pageID := vars["id"]
+  thisPage := Page{}
+  // Scan translates the results of a query to a struct
+  err := database.QueryRow("SELECT page_title, page_content, page_date FROM pages WHERE id=?", pageID).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date)
+  if err != nil {
+    log.Println("Could not get page: " + pageID)
+    log.Println(err.Error())
+  }
+  html := `<html><head><title>` + thisPage.Title + `</title></head></body><h1>` + thisPage.Title + `</h1><div>` + thisPage.Content + `</div></body></html>`
+  fmt.Fprintln(w, html)
+}
+
+func main() {
+  dbConn := fmt.Sprintf("%s:%s@/%s", DBUser, DBPass, DBDbase)
+  fmt.Println(dbConn)
+  db, err := sql.Open("mysql", dbConn)
+  if err != nil {
+    log.Println("Could not connect to " + DBDbase)
+    log.Println(err.Error)
+  }
+  database = db
+
+  routes := mux.NewRouter()
+  routes.HandleFunc("/page/{id:[0-9]+}", ServePage)
+  http.Handle("/", routes)
+  http.ListenAndServe(PORT, nil)
+}
